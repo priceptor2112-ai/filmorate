b0403aeafec7ab331ef234a164b90c9aa80dfbed
priceptor2112-ai <priceptor2112@gmail.com>
2026-04-12 18:39:00 +0300

    fix: recreate EventDbStorage with correct encoding

--- src/main/java/ru/yandex/practicum/filmorate/storage/EventDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EventDbStorage.java
@@ -1,4 +1,4 @@
-﻿package ru.yandex.practicum.filmorate.storage;
+package ru.yandex.practicum.filmorate.storage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
